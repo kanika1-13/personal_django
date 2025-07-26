d1298ad4bb4de3cc1c00de34b02df224e6fb89eb
kanika1-13 <kanika22102@iiitnr.edu.in>
2025-07-26 22:45:53 +0530

    final touches - fixing images in deployement

--- django_project/studybud/urls.py
+++ django_project/studybud/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/',include('base.api.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
